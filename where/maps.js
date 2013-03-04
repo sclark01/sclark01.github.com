cc5aa98e381d5369f955efda6e1337f336b3238a
sclark01 <sclark.tieche@gmail.com>
2013-03-04 00:57:41 -0500

    trying to fix infowindow bugs

--- where/maps.js
+++ where/maps.js
@@ -16,6 +16,8 @@ function run() {
 function get_location(){
 	if (navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(showPosition);
+		var x = showPosition.myLatLng;
+		alert(x);
 		}
 	else {
 		alert("GeoLocation is not enabled on this device");
@@ -28,15 +30,6 @@ function showPosition(position){
 	var myInfo = new google.maps.InfoWindow({
 		content: "HERE I AM"
 	});
-	
-		var mapOptions = {
-          center: new google.maps.LatLng(42.317939,-71.081543),
-          zoom: 11,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-            mapOptions);
 	
 	var marker = new google.maps.Marker({
 		position: myLatLng,
