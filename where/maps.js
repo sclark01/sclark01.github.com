e8bca8b4748751710b00d074eab3106fc71f311b
sclark01 <sclark.tieche@gmail.com>
2013-03-04 02:18:14 -0500

    added station

--- where/maps.js
+++ where/maps.js
@@ -1,5 +1,5 @@
 var map;
-var stations;
+var stations = [];
 
 function run() {
 
@@ -11,8 +11,8 @@ function run() {
 function draw_map() {
 	
 	var mapOptions = {
-          center: new google.maps.LatLng(42.3583, -71.0603),
-          zoom: 12,
+          center: new google.maps.LatLng(42.356403,-71.062596),
+          zoom: 11,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         
@@ -48,12 +48,13 @@ function showPosition(position){
 
 function draw_station(){
 	station_coordinates();
-	var stop;
-	for (i = 0; i < 4; i++){
+	var num_stops = 21;
+	var stop = [];
+	for (i = 0; i < num_stops; i++){
 		stop[i] = new google.maps.Marker({
 			position: stations[i],
 			map: map,
-			title: "station"
+			title: ""
 		});
 	}
 	
@@ -65,24 +66,24 @@ function station_coordinates(){
 	stations[1] = new google.maps.LatLng(42.396967,-71.122935);
 	stations[2] = new google.maps.LatLng(42.388441,-71.119266);
 	stations[3] = new google.maps.LatLng(42.373534,-71.118976);
-	/*
-	stations[4] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[5] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[6] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[7] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[8] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[9] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[10] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[11] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[12] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[13] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[14] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[15] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[16] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[17] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[18] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[19] = new google.maps.LatLng(42.399874,-71.137996);
-	stations[20] = new google.maps.LatLng(42.399874,-71.137996);*/
+	stations[4] = new google.maps.LatLng(42.36552,-71.104116);
+	stations[5] = new google.maps.LatLng(42.362413,-71.086178);
+	stations[6] = new google.maps.LatLng(42.361398,-71.071029);
+	stations[7] = new google.maps.LatLng(42.356356,-71.062188);
+	stations[8] = new google.maps.LatLng(42.355499,-71.06036);
+	stations[9] = new google.maps.LatLng(42.352312,-71.055343);
+	stations[10] = new google.maps.LatLng(42.342559,-71.057038);
+	stations[11] = new google.maps.LatLng(42.330147,-71.057392);
+	stations[12] = new google.maps.LatLng(42.320668,-71.052189);
+	stations[13] = new google.maps.LatLng(42.311275,-71.053391);
+	stations[14] = new google.maps.LatLng(42.30004,-71.06163);
+	stations[15] = new google.maps.LatLng(42.293088,-71.065793);
+	stations[16] = new google.maps.LatLng(42.284612,-71.064205);
+	stations[17] = new google.maps.LatLng(42.276039,-71.029444);
+	stations[18] = new google.maps.LatLng(42.266448,-71.020346);
+	stations[19] = new google.maps.LatLng(42.251774,-71.004896);
+	stations[20] = new google.maps.LatLng(42.233347,-71.007128);
+	stations[21] = new google.maps.LatLng(42.208081,-71.001248);
 }
 
 
