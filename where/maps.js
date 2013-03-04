e5a30e15ba7f61d8682592bfc5f7e5202df5f480
sclark01 <sclark.tieche@gmail.com>
2013-03-04 15:34:32 -0500

    trying to find closest station

--- where/maps.js
+++ where/maps.js
@@ -122,7 +122,7 @@ function find_distance(lat1, lon1){
 	for(i = 0; i < 13; i++) {
 		lat2 = stations_all[i].lat();
 		lon2 = stations_all[i].lng();
-		temp = haversine(lat1, lon1, lat2, lon2);
+		console.log(haversine(lat1, lon1, lat2, lon2));
 		console.log(temp);
 		if (temp < dis){
 			dis = temp;
