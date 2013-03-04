3e3981ccbd00a53cd0e7a17c523e3f90572767b8
sclark01 <sclark.tieche@gmail.com>
2013-03-03 19:31:23 -0500

    trying to get geolocation to function

--- where/maps.js
+++ where/maps.js
@@ -1,23 +1,24 @@
 function run() {
         var mapOptions = {
           center: new google.maps.LatLng(42.317939,-71.081543),
-          zoom: 10,
+          zoom: 11,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"),
             mapOptions);
-            get_location();
+        get_location();
 
 }
 
 function get_location(){
 	if (navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(position)){
-			myLat = position.coords.latitude;
-			myLng = position.coords.longitude;
-		});
-	}
+		navigator.geolocation.getCurrentPosition(showPosition);
+		}
 	else {
 		alert("GeoLocation is Not Enabled on this device");
 	}
+}
+
+function showPosition(position){
+	alert("Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude);
 }
