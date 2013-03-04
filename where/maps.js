7383afaf41c1d10d9c3ac113c6aaf72dd9d574cd
sclark01 <sclark.tieche@gmail.com>
2013-03-04 12:31:08 -0500

    adjusting logo size

--- where/maps.js
+++ where/maps.js
@@ -146,10 +146,10 @@ function haversine(lat1, lon1, lat2, lon2){
 
 	var R = 6371;
 	var dLat = (lat2 - lat1);
-	dLat = dLat.toRad();
-	var dLon = (lon2 - lon1).toRad();
-	var lat1 = lat1.toRad();
-	var lat2 = lat2.toRad();
+	
+	var dLon = (lon2 - lon1);
+	var lat1 = lat1;
+	var lat2 = lat2;
 	
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
